62adf1e665e8cdadfdb627cc7cae97f5d9268e49
Gexwolf <sebastian.posada@est.iudigital.edu.co>
2022-03-13 15:12:35 -0500

    Launch json

--- Actividad 2/Main.java
+++ Actividad 2/Main.java
@@ -12,8 +12,10 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args){
       
-       // Se estblece un array de tipo Persona
+       
+     // Se establece un array de tipo Persona
         Persona personaArray[] = new Persona[5];
+        
         Persona persona;
         
         // Método constructor del scanner para recibir input del usuario
