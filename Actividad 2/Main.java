f2cb85e7743fef2508f523cc7543979a031c4dcd
PROX <sebastian.posada@est.iudigital.edu.co>
2022-03-20 00:38:05 -0500

    orden y comentarios
    Mejor organización y comentarios más completos

--- Actividad 2/Main.java
+++ Actividad 2/Main.java
@@ -2,7 +2,7 @@
  * Ejecicio Actividad 2, Programación Orientada a Objetos
  * IU Digital de Antioquia
  *
- * 11/03/2022
+ * 20/03/2022
  * 
  * @author Sebastian Posada Moreno
  */
@@ -10,89 +10,75 @@
 import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args){
+  public static void main(String[] args){
 
-      // Método constructor del scanner para recibir input del usuario
-      Scanner userInput = new Scanner(System.in);
+    // Método constructor del scanner para recibir input del usuario
+    Scanner userInput = new Scanner(System.in);
 
-      //Se ingresa la cantidad de personas a registrar
-      
-      System.out.println("¡Bienvenido! Para comenzar, por favor indique la cantidad de personas que desea registrar: ");
-      int cantidadPersonas = userInput.nextInt();
-      
-      
+    //Se ingresa la cantidad de personas a registrar      
+    System.out.println("\n" + "¡Bienvenido! Para comenzar, por favor indique la cantidad de personas que desea registrar: ");
+    int cantidadPersonas = userInput.nextInt();
 
-      
-       // Se estblece un array de tipo Persona
-      Persona[] personaArray = new Persona[cantidadPersonas];
-      Persona persona;
-        
-      float edad = 0;
-      float totalEdades = 0;
-      
-      for (int i = 0; i < cantidadPersonas ; i++) {
+    // Se establece un array de tipo Persona
+    Persona[] personaArray = new Persona[cantidadPersonas];
+    Persona persona;
 
-              
-              persona = new Persona();
-  
-   // Solicitamos al usuario los datos de persona1
-              System.out.println("Por favor, ingrese su nombre:");
-              String nombre = userInput.next();
-              persona.setNombre(nombre);
-
-              System.out.println("Hola "+nombre+", ¿Cual es su apellido?:");
-              String apellido = userInput.next();
-              persona.setApellido(apellido);
-
-              System.out.println("Generando registro para "+nombre+" "+apellido+", ¿es usted Mujer u Hombre?:");
-              String genero = userInput.next();
-              persona.setGenero(genero);
-
-              System.out.println("Muchas gracias, por último, ingrese su edad en números:");
-              edad = userInput.nextFloat();
-              persona.setEdad(edad); 
-              
-
-              System.out.println("Registro #"+i+" exitoso para "+nombre+" "+apellido+", "+genero+" de "+edad+" años.");
-            
-            
-            personaArray[i] = persona;
-            totalEdades += personaArray[i].getEdad();
-            
-            
-          
-      }
-
-      
-      
-      
-      // Se informa registro exitoso para las 5 personas y se muestra el listado en consola
-      System.out.println("Se ha completado el registro. Las personas ingresadas son: ");
-
-      for (int i = 0; i < cantidadPersonas; i++){
-
-        System.out.println(personaArray[i].getNombre() + " " + personaArray[i].getApellido() + ", " + personaArray[i].getGenero() + " de " + personaArray[i].getEdad() + " años.");
-
-      }
-
-      // Imprime en consola el promedio de las edades, utilizando el metodo promedioEdades()
-      System.out.println("El promedio de edades es: " + promedioEdades(totalEdades, cantidadPersonas));
-      
-
-      userInput.close();
-      
-    }
-
+    // se declara variable a tener en cuenta al momento de promediar edades  
+    float totalEdades = 0;
     
+    // Solicitamos al usuario los datos de las personas
+    for (int i = 0; i < cantidadPersonas; i++){
+            
+      persona = new Persona();
 
+      System.out.println("Por favor, ingrese su nombre:");
+      String nombre = userInput.next();
+      persona.setNombre(nombre);
 
+      System.out.println("Hola "+nombre+", ¿Cual es su apellido?:");
+      String apellido = userInput.next();
+      persona.setApellido(apellido);
 
+      System.out.println("Generando registro para "+nombre+" "+apellido+", ¿es usted Mujer u Hombre?:");
+      String genero = userInput.next();
+      persona.setGenero(genero);
 
-    public static double promedioEdades(float totalEdades, int cantidadPersonas){
+      System.out.println("Muchas gracias, por último, ingrese su edad en números:");
+      float edad = userInput.nextFloat();
+      persona.setEdad(edad);       
 
-      return totalEdades / cantidadPersonas;
+      System.out.println("\n" + "Registro #" + i + " exitoso para " + nombre + " " + apellido + ", " + genero + " de " + edad + " años.");          
+          
+      // Asigna los valores de la persona establecidos anteriormente y los guarda en la posición "i" del array personaArray
+      personaArray[i] = persona;
+
+      // Suma en cada ciclo cada una de las edades ingresadas, para ser usado al momento de sacar el promedio
+      totalEdades += personaArray[i].getEdad();          
+        
+    }   
+    
+    // Se informa registro exitoso para las personas ingresadas y se muestra el listado en consola
+    System.out.println("\n"+"Se ha completado el registro. Las personas ingresadas son: "+"\n");
+    
+    for (int i = 0; i < cantidadPersonas; i++){
+
+      System.out.println(personaArray[i].getNombre() + " " + personaArray[i].getApellido() + ", " + personaArray[i].getGenero() + " de " + personaArray[i].getEdad() + " años.");
 
     }
 
+    // Imprime en consola el promedio de las edades, utilizando el metodo promedioEdades()
+    System.out.println("\n" + "El promedio de edades es: " + promedioEdades(totalEdades, cantidadPersonas) + "\n");
+    
+    // Se cierra el input de usuario para evitar leaks de memoria
+    userInput.close();
+      
+  }
+
+  // método que toma la suma de las edades de todas las personas registradas, y las divide ente la cantidad de estas
+  public static double promedioEdades(float totalEdades, int cantidadPersonas){
+
+    return totalEdades / cantidadPersonas;
+
+  }
     
 }
