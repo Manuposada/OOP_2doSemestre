848de4cb9d18bbb0fcff5be5ec67c186fdcb356a
Manuposada <101668755+Manuposada@users.noreply.github.com>
2022-03-21 13:12:57 -0300

    Manuelizada
    Manuelicé el trabajo

--- Actividad 2/Main.java
+++ Actividad 2/Main.java
@@ -1,11 +1,12 @@
 /**
- * Ejecicio Actividad de aprendizaje 2
- * Programación Orientada a Objetos
+ * Solucion Actividad de aprendizaje 2
+ * Programación Orientada a Objetos 1
+ * Tecnología en Desarrollo de Software
  * IU Digital de Antioquia
  *
- * 20/03/2022
+ * 21/03/2022
  * 
- * @author Sebastian Posada Moreno
+ * @author Manuela Posada Moreno
  */
 
 import java.util.Scanner;
@@ -13,102 +14,102 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args){
 
-    // Método constructor del scanner para recibir input del usuario
-    Scanner userInput = new Scanner(System.in);
+    // Método constructor para recibir lo que escribió el usuario
+    Scanner datos=new Scanner(System.in);
 
-    //Se ingresa la cantidad de personas a registrar      
-    System.out.println("\n" + "¡Bienvenido! Para comenzar, por favor indique la cantidad de personas que desea registrar: ");
-    int cantidadPersonas = userInput.nextInt();
+    //Cantidad de personas a registrar      
+    System.out.println("\n" + "¡Hola! Por favor indique el número de personas que va a registrar: ");
+    int numPer=datos.nextInt();
 
-    // Se establece un array de tipo Persona
-    Persona[] personaArray = new Persona[cantidadPersonas];
+    // Creamos un array de tipo Persona
+    Persona[] pArray=new Persona[numPer];
     Persona persona;
 
-    // se declara variable a tener en cuenta al momento de promediar edades  
-    float totalEdades = 0;
+    // Delaramos variable para  
+    float totalE = 0;
     
-    // Solicitamos al usuario los datos de las personas
-    for (int i = 0; i < cantidadPersonas; i++){
+    // Pedimoa los datos de las personas
+    for (int i=0; i < numPer; i++){
             
       persona = new Persona();
 
-      System.out.println("\n" + "Por favor, ingrese su nombre:");
-      String nombre = userInput.next();
-      persona.setNombre(nombre);
+      System.out.println("\n" + "Por favor, diganos su nombre:");
+      String nom=datos.next();
+      persona.setNom(nom);
 
-      System.out.println("Hola "+nombre+", ¿Cual es su apellido?:");
-      String apellido = userInput.next();
-      persona.setApellido(apellido);
+      System.out.println("Bienvenido "+nom+", ¿Cual es su apellido?:");
+      String ape=datos.next();
+      persona.setApe(ape);
 
-      System.out.println("Generando registro para "+nombre+" "+apellido+", ¿es usted Hombre / Mujer?:");
-      String genero = userInput.next();
-      persona.setGenero(genero);
+      System.out.println("Estamos creando el registro para "+nom+" "+ape+", ¿usted es Hombre / Mujer?:");
+      String gen=datos.next();
+      persona . setGen(gen);
 
-      System.out.println("Muchas gracias, por último, ingrese su edad en números:");
-      float edad = userInput.nextFloat();
+      System.out.println("Muchas gracias "+nom+", para terminar, por favor escriba su edad en números:");
+      float edad=datos.nextFloat();
       persona.setEdad(edad);      
 
-      System.out.println("\n" + "Registro #" + i + " exitoso para " + nombre + " " + apellido + ", " + genero + " de " + edad + " años.");
+      System.out.println("\n" + "El registro #"+i+" para "+nom+" "+ape+", "+gen+" de "+edad+" años, fue creado exitosamente.");
       System.out.println("\n" + "Nuevo registro");          
           
-      // Asigna los valores de la persona establecidos anteriormente y los guarda en la posición "i" del array personaArray
-      personaArray[i] = persona;
+      // Se asignan los valores de la persona anterior y los guarda en la posición "i" del array pArray
+      pArray[i] = persona;
 
-      // Suma en cada ciclo cada una de las edades ingresadas, para ser usado al momento de sacar el promedio
-      totalEdades += personaArray[i].getEdad();          
+      // Suma en cada ciclo las edades ingresadas para sacar el promedio
+      totalE += pArray[i].getEdad();          
         
     }   
     
-    // Se informa registro exitoso para las personas ingresadas y se muestra el listado en consola
-    System.out.println("\n"+"Se ha completado el registro. Las personas ingresadas son: "+"\n");
+    // Se informa registro exitoso y se muestra el listado
+    System.out.println("\n"+"Registro completado. Las personas registradas son: "+"\n");
     
-    for (int i = 0; i < cantidadPersonas; i++){
+    for (int i = 0; i < numPer; i++){
 
-      System.out.println(personaArray[i].getNombre() + " " + personaArray[i].getApellido() + ", " + personaArray[i].getGenero() + " de " + personaArray[i].getEdad() + " años.");
+      System.out.println(pArray[i].getNom() + " " + pArray[i].getApe() + ", " + pArray[i].getGen() + " de " + pArray[i].getEdad() + " años.");
 
     }
 
-    // Imprime en consola el promedio de las edades, utilizando el metodo promedioEdades()
-    System.out.println("\n" + "El promedio de edades es: " + promedioEdades(totalEdades, cantidadPersonas) + "\n");
+    // Imprime el promedio de las edades, utilizando el metodo prEdad()
+    System.out.println("\n" + "El promedio de edades es: " + prEda(totalE, numPer) + "\n");
 
-    // Se llama método para imprimir en consola la cantidad de Hombres y Mujeres registrados
-    printCantidadDeHombresYMujeres(cantidadPersonas, personaArray);
+    // Se llama al método para imprimir la cantidad de Hombres y Mujeres
+    printCantHYM(numPer, pArray);
 
-    // Se cierra el input de usuario para evitar fugas de memoria
-    userInput.close();
+    // Se cierra el input de usuario
+    datos.close();
 
   }
 
 
-  // Método que toma la suma de las edades de todas las personas registradas, y las divide ente la cantidad de estas
-  public static double promedioEdades(float totalEdades, int cantidadPersonas){
+  // Toma la suma de las edades y las divide ente la cantidad de personas
+  public static double prEda(float totalE, int numPer){
 
-    return totalEdades / cantidadPersonas;
+    return totalE / numPer;
 
   }
 
   // Método para retornar la cantidad de Hombres y Mujeres
 
-  public static void printCantidadDeHombresYMujeres(int cantidadPersonas, Persona[] personaArray){
+  public static void printCantHYM(int numPer, Persona[] pArray){
 
-    int cantidadHombres = 0;
-    int cantidadMujeres = 0;
+    int cantH = 0;
+    int cantM = 0;
     int generoNoDefinido = 0;
-    String Hombre = "Hombre";
-    String Mujer = "Mujer";
+    String H = "Hombre";
+    String M = "Mujer";
 
-    for (int i = 0; i < cantidadPersonas; i++){
+    for (int i = 0; i < numPer; i++){
 
-      String genero = personaArray[i].getGenero();
-      if (genero.equals(Hombre)){
+      String gen = pArray[i].getGen();
+      if (gen.equals(H)){
 
-        cantidadHombres += 1; 
+        cantH += 1; 
 
       }
 
-      else if (genero.equals(Mujer)){
+      else if (gen.equals(M)){
 
-        cantidadMujeres += 1;
+        cantM += 1;
 
       }
 
@@ -119,9 +120,9 @@ class Main {
       }
     }
 
-    System.out.println("La cantidad de Hombres y Mujeres ingresados es:");
-    System.out.println("Hombres: " + cantidadHombres);
-    System.out.println("Mujeres: " + cantidadMujeres);
+    System.out.println("La cantidad de Hombres y Mujeres es:");
+    System.out.println("Hombres: " + cantH);
+    System.out.println("Mujeres: " + cantM);
     System.out.println("Género no definido: " + generoNoDefinido + "\n");
   } 
 }
--- Actividad 2/Persona.java
+++ Actividad 2/Persona.java
@@ -1,55 +1,56 @@
 /**
- * Ejecicio Actividad de aprendizaje 2
- * Programación Orientada a Objetos
+ * Solución Actividad de aprendizaje 2
+ * Programación Orientada a Objetos 1
+ * Tecnología en Desarrollo de software
  * IU Digital de Antioquia
  *
- * 20/03/2022
+ * 21/03/2022
  * 
- * @author Sebastian Posada Moreno
+ * @author Manuela Posada Moreno
  */
 
 class Persona {
 
     /**
-     * Atributos que definen a la clase Persona
+     * Atributos para la clase Persona
      */
-        private String nombre;
-        private String apellido;
-        private String genero;
+        private String nom;
+        private String ape;
+        private String gen;
         private float edad;
         
     /**
-     * Métodos de la clase
+     * Métodos de la clase Persona
      */
 
         // Para editar el campo "nombre"
-        public void setNombre(String nombreIngresado){
-            this.nombre = nombreIngresado;
+        public void setNom(String nomIng){
+            this.nom = nomIng;
         }
         
         // Para obtener el campo "Nombre"
-        public String getNombre(){
-            return this.nombre;
+        public String getNom(){
+            return this.nom;
         }
 
         // Para editar el campo "apellido"
-        public void setApellido(String apellidoIngresado){
-            this.apellido = apellidoIngresado;
+        public void setApe(String apeIng){
+            this.ape = apeIng;
         }
         
-        // Para obtener el campo "apellido"
-        public String getApellido(){
-            return this.apellido;
+        // Para obtener el campo "Apellido"
+        public String getApe(){
+            return this.ape;
         }
 
         // Para editar el campo "genero"
-        public void setGenero(String generoIngresado){
-            this.genero = generoIngresado;
+        public void setGen(String genIng){
+            this.gen = genIng;
         }
         
-        // Para obtener el campo "genero"
-        public String getGenero(){
-            return this.genero;
+        // Para obtener el campo "Genero"
+        public String getGen(){
+            return this.gen;
         }
 
         // Para editar el campo "edad"
@@ -57,7 +58,7 @@ class Persona {
             this.edad = edadIngresada;
         }
 
-        // Para obtener el campo "edad"
+        // Para obtener el campo "Edad"
         public float getEdad(){
             return this.edad;
         }
